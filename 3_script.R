8205fb179259344c731dc3e4d1e79994a47e55d9
pedrofortesgonzalez <pedrofortesgonzalez94@gmail.com>
2025-01-20 10:42:54 +0100

    Update 3_script.R

--- 3_script.R
+++ 3_script.R
@@ -1,26 +1,38 @@
+# 1-PRE-PROCESADO --------------------------------------------------------------
+# 1.1.Set WEnV -----------------------------------------------------------------
+rm(list=ls()) # resetear WEnv
+path <- "/Users/pedrofortesgonzalez/Desktop/MASTER_UNIR/11_ALGORITMOS_IA/actividades/Act3_grupal/Material complementario"
+setwd(path)
+
+# Librerías y random-seed-------------------------------------------------------
+set.seed(1999)
 library(readr)
-#Estructurar el Dataframe
-clases <- read.csv("1_data/classes.csv", header = FALSE, sep = ";", col.names = c("Muestra", "Clase"), row.names = 1)
+library(dplyr)
+library(caret)
+library(randomForest)
+library(klaR)
+library(glmnet)
+library(pROC)
+
+# Estructurar el Dataframe a partir de Material_complementario------------------
+clases <- read.csv("./1_data/classes.csv", header = FALSE, sep = ";", col.names = c("Muestra", "Clase"), row.names = 1)
 columnas <- read_lines("1_data/column_names.txt")
 gen_exp <-read.csv("1_data/gene_expression.csv", header = FALSE, sep = ";", col.names = (columnas))
-datos <- cbind(clases, gen_exp)
-#Imputación
-sum(is.na(datos)) #En principio no hay NAs
-datos$Clase <- as.factor(datos$Clase)
-str(datos)
-ceros <- colnames(datos[,2:501])[colSums(datos[,2:501]) ==0] #Que genes dan 0
-datos_imputados <- datos[, !colnames(datos) %in% ceros] #Quitamos ceros
-datos_imputados <- datos[, colSums(datos[,2:501]) != 0] #Quitamos ceros (más simple)
-datos #Ha removido 3 variables
+df <- cbind(clases, gen_exp)
+df$Clase <- as.factor(df$Clase) # convertir variable clase a factor
 
-#######################Aporte Cris#############################################
-#al ejecutar esto
-any(colSums(datos_imputados[ ,sapply(datos_imputados,is.numeric)]) == 0) ##sigue habiendo columnas con 0
-genes <- gen_exp[ ,colSums(gen_exp) != 0] #sobre df con todo numeros, quitamos las columnas con todo 0
-genes <- scale(genes) #escalamos tambien
-df <- cbind(clases, genes)
-any(is.na(df)) #no datos faltantes
-any(colSums(df[ ,sapply(df,is.numeric)]) == 0) #ya no hay columnas con todo 0
-dim(df) #ahora si que se han eliminado las variables con todo 0
-write.csv(df,"1_data/df.csv")
+# Procesar Valores faltantes----------------------------------------------------
+any(sum(is.na(df))) #En principio no hay NAs
+any(colSums(df[ ,sapply(df,is.numeric)]) == 0) # Pero sí que hay columnas con todo 0s
+ceros <- colnames(df[,2:501])[colSums(df[,2:501]) ==0] # Las almacenamos aquí por si queremos verlas
+df <- df[, !colnames(df) %in% ceros] # Y las eliminamos ya que no aportan información y tenemos muchas otras aún por analizar
+any(colSums(df[ ,sapply(df,is.numeric)]) == 0) #Comprobamos que ya no hay columnas con todo 0
+write.csv(df,"1_data/1_df.csv") # Y lo exportamos (por si acaso)
+
+# Dimensiones del df------------------------------------------------------------
+dim(df)
+
+## Tenemos 801 filas y 498 columnas = alta dimensionalidad.
+## Debemos escoger un método para elegir columnas ya que, si no, tendremos mucha info (y por tanto ruido) en nuestro análisis
+## Probamos a aplicar LASSO para quedarnos con el subconjunto óptimo de variables
 
